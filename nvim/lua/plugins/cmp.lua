830e030faff811f5e1789cd0e9cef631d608f2b6
Andranik <tednaaa@gmail.com>
2024-06-21 16:37:40 +0400

    nvim: update conform plugin

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,88 @@
+return {
+	{
+		"Exafunction/codeium.nvim",
+		cmd = "Codeium",
+		build = ":Codeium Auth",
+		opts = {},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "onsails/lspkind.nvim" },
+
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "rafamadriz/friendly-snippets" },
+			{ "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+			{ "windwp/nvim-autopairs" },
+		},
+		config = function()
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+			local luasnip = require("luasnip")
+			local autopairs = require("nvim-autopairs")
+
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+					["<C-CR>"] = function(fallback)
+						cmp.abort()
+						fallback()
+					end,
+				}),
+				completion = {
+					completeopt = "menu,menuone,preview,noselect",
+				},
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				---@diagnostic disable-next-line: missing-fields
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						maxwidth = 20,
+						ellipsis_char = "...",
+						show_labelDetails = true,
+						symbol_map = { Codeium = "" },
+
+						before = function(_, vim_item)
+							return vim_item
+						end,
+					}),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "codeium" },
+				},
+			})
+
+			autopairs.setup({
+				check_ts = true,
+				ts_config = {
+					lua = { "string" },
+					javascript = { "template_string" },
+					java = false,
+				},
+			})
+
+			cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+		end,
+	},
+}
