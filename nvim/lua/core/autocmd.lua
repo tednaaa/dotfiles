0d6edd688a857e14cc5fe8763b08caaf6b447243
Andranik <tednaaa@gmail.com>
2024-06-21 16:39:55 +0400

    nvim: remove LspAttach autocmd.lua

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -3,12 +3,12 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 	command = "set nopaste",
 })
 
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = { "json", "jsonc", "markdown" },
--- 	callback = function()
--- 		vim.opt.conceallevel = 0
--- 	end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
+})
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking copying text",
@@ -16,28 +16,4 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("LspConfig", {}),
-	callback = function(event)
-		vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-		local opts = { buffer = event.buf }
-
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-
-		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-	end
 })
