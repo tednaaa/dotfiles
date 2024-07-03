75220113e93abdc51960fd95b51a1fba56ab9bb1
Andranik <tednaaa@gmail.com>
2024-07-03 15:06:49 +0400

    nvim(conform): update configuration

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,29 +1,16 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettierd" },
-				typescript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				svelte = { "prettierd" },
-				css = { "prettierd" },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				yaml = { "prettierd" },
-				markdown = { "prettierd" },
-				graphql = { "prettierd" },
-				liquid = { "prettierd" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		})
-	end,
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "eslint_d", "prettierd" },
+			typescript = { "eslint_d", "prettierd" },
+			javascriptreact = { "eslint_d", "prettierd" },
+			typescriptreact = { "eslint_d", "prettierd" },
+			json = { "prettierd" },
+		},
+		format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+	},
 }
