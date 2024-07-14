103688f86576565353a378452b855e0d97099f08
Andranik <tednaaa@gmail.com>
2024-07-14 15:09:22 +0400

    nvim: little bit refactoring and improve highlighting

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -61,6 +61,7 @@ return {
 
 					format = function(entry, item)
 						local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+
 						item = require("lspkind").cmp_format({
 							mode = "symbol",
 							maxwidth = 20,
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,23 +3,10 @@ return {
 	{
 		"brenoprata10/nvim-highlight-colors",
 		config = function()
-			require("nvim-highlight-colors").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			require("mini.ai").setup({ n_lines = 500 })
-
-			require("mini.surround").setup()
-
-			local statusline = require("mini.statusline")
-
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
+			require("nvim-highlight-colors").setup({
+				virtual_symbol = "●",
+				render = "virtual",
+			})
 		end,
 	},
 }
