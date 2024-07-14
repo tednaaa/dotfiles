fdee6f4b8477ea1ca99199fcd9b3a484b1871445
Andranik <tednaaa@gmail.com>
2024-07-14 14:20:31 +0400

    nvim: update neotree layout

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -10,10 +10,14 @@ return {
 		},
 		keys = { { "<leader>e", "<cmd>Neotree toggle<cr>" } },
 		config = function()
+			vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+			vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+			vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+			vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
 			require("neo-tree").setup({
-				close_if_last_window = true,
 				source_selector = {
-					winbar = false,
+					winbar = true,
 					content_layout = "center",
 					show_scrolled_off_parent_node = false,
 				},
@@ -25,6 +29,21 @@ return {
 						never_show = { ".DS_Store" },
 					},
 					follow_current_file = { enabled = true },
+				},
+				default_component_configs = {
+					git_status = {
+						symbols = {
+							added = "",
+							modified = "",
+							deleted = "✖",
+							renamed = "󰁕",
+							untracked = "",
+							ignored = "",
+							unstaged = "󰄱",
+							staged = "",
+							conflict = "",
+						},
+					},
 				},
 			})
 
