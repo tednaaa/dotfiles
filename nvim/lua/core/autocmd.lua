66ced46e3933db48d8a89bc8251c7d9fd1a2ff8f
Andranik <tednaaa@gmail.com>
2024-07-19 18:43:50 +0400

    nvim: remove some autocmd

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -1,8 +1,3 @@
-vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
-})
-
 vim.api.nvim_create_autocmd({ "CmdlineLeave", "VimEnter" }, {
 	callback = function()
 		vim.opt.pumheight = 15
@@ -14,13 +9,13 @@ vim.api.nvim_create_autocmd("CmdlineEnter", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "json", "jsonc", "markdown" },
-	callback = function()
-		vim.opt.conceallevel = 0
-	end,
-})
-
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = { "json", "jsonc", "markdown" },
+-- 	callback = function()
+-- 		vim.opt.conceallevel = 0
+-- 	end,
+-- })
+--
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking copying text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
