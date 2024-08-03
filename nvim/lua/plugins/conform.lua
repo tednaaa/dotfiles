64558b90eb435fbca763b006d58e97646584c16e
Andranik <tednaaa@gmail.com>
2024-08-04 00:20:43 +0400

    nvim(conform): temporary disable async and set 3000 timeout

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,16 +1,14 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufEnter" },
-	cmd = { "ConformInfo" },
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			javascript = { "eslint_d", "prettierd" },
-			typescript = { "eslint_d", "prettierd" },
-			javascriptreact = { "eslint_d", "prettierd" },
-			typescriptreact = { "eslint_d", "prettierd" },
+			typescript = { "prettierd", "eslint_d" },
+			typescriptreact = { "prettierd", "eslint_d" },
+			javascript = { "prettierd", "eslint_d" },
+			javascriptreact = { "prettierd", "eslint_d" },
 			json = { "prettierd" },
 		},
-		format_on_save = { timeout_ms = 500, lsp_format = "fallback", async = true },
+		format_on_save = { timeout_ms = 3000, lsp_format = "fallback" },
 	},
 }
