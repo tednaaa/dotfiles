e986e86f0dd25e256ddadd4b4b263c97727c618c
Andranik <tednaaa@gmail.com>
2024-08-04 00:21:27 +0400

    nvim: add trouble plugin and move neo-tree to single file

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,51 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	keys = { { "<leader>e", "<cmd>Neotree toggle<cr>" } },
+	config = function()
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+		require("neo-tree").setup({
+			source_selector = {
+				winbar = false,
+				show_scrolled_off_parent_node = false,
+			},
+
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false,
+					never_show = { ".DS_Store" },
+				},
+				follow_current_file = { enabled = true },
+			},
+			default_component_configs = {
+				git_status = {
+					symbols = {
+						added = "",
+						modified = "",
+						deleted = "󰚃",
+						renamed = "󰛿",
+						untracked = "○",
+						ignored = "◌",
+						unstaged = "○",
+						staged = "●",
+						conflict = "◎",
+					},
+				},
+			},
+		})
+
+		vim.cmd("Neotree show")
+	end,
+}
---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/trouble.nvim",
+	opts = {},
+	cmd = "Trouble",
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+		{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Info (Trouble)" },
+	},
+}
