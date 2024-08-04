c85a1cae1fde7112938016b25f34e44ced58e6bb
Andranik <tednaaa@gmail.com>
2024-08-05 00:01:18 +0400

    nvim: create user command to generate editorconfig

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,9 +3,14 @@ require("core.keymaps")
 require("core.autocmd")
 
 local clear_processes = require("modules.clear-processes")
+local generate_editorconfig = require("modules.generate-editorconfig")
 
 vim.api.nvim_create_user_command("ClearProcesses", function()
 	clear_processes.clear_processes()
+end, {})
+
+vim.api.nvim_create_user_command("GenerateEditorconfig", function()
+	generate_editorconfig.generate_editorconfig()
 end, {})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
