bea4d7c0e44e4806c5dfb0551663a4956d45bf6b
Andranik <tednaaa@gmail.com>
2024-08-11 21:20:17 +0400

    fish: add golang asdf settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,14 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
 end
+set -x XDG_CONFIG_HOME $HOME/.config
+
+source ~/.asdf/asdf.fish
+source "$HOME/.cargo/env.fish"
+source ~/.asdf/plugins/golang/set-env.fish
+
+set -x ASDF_GOLANG_MOD_VERSION_ENABLED true
 
 starship init fish | source
 fzf --fish | source
 
-source ~/.asdf/asdf.fish
-source "$HOME/.cargo/env.fish"
-
-set -x XDG_CONFIG_HOME $HOME/.config
