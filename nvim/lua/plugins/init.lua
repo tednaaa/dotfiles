18972171be7be27031fe305504b343fb598fedb3
Andranik <tednaaa@gmail.com>
2024-08-11 21:24:13 +0400

    nvim: add ts-comments for better commenting & vim-be-good

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
 	{ "wakatime/vim-wakatime" },
 	{ "RRethy/vim-illuminate" },
+	{ "folke/ts-comments.nvim", opts = {}, event = "VeryLazy", enabled = true },
+	{ "ThePrimeagen/vim-be-good", event = "VeryLazy" },
 
 	{
 		"brenoprata10/nvim-highlight-colors",
