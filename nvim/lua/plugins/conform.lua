dbaacc53ce4bb4080d04e0bf0a8dc7e8693c05ba
Andranik <tednaaa@gmail.com>
2024-08-12 22:50:59 +0400

    nvim(conform): add vue files formatting with prettierd & eslint_d

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
 			typescriptreact = { "prettierd", "eslint_d" },
 			javascript = { "prettierd", "eslint_d" },
 			javascriptreact = { "prettierd", "eslint_d" },
+			vue = { "prettierd", "eslint_d" },
 			json = { "prettierd" },
 		},
 		format_on_save = { timeout_ms = 1000, lsp_format = "fallback" },
