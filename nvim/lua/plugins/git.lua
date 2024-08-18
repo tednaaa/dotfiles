431b522b85bf55d73d2e0772f10b4d1a4db74248
Andranik <tednaaa@gmail.com>
2024-08-19 01:01:08 +0400

    nvim: configure diffview plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -8,7 +8,24 @@ return {
 			"sindrets/diffview.nvim",
 			"ibhagwan/fzf-lua",
 		},
-		config = true,
+		config = function()
+			local diffview = require("diffview")
+			local actions = require("diffview.actions")
+
+			diffview.setup({
+				keymaps = {
+					file_panel = {
+						{ "n", "j", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+						{ "n", "k", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
+					},
+				},
+			})
+
+			require("neogit").setup({ integrations = { diffview = true, fzf_lua = true } })
+
+			vim.keymap.set("n", "gvo", diffview.open)
+			vim.keymap.set("n", "gvc", diffview.close)
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
