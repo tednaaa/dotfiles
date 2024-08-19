d5e8c9c8c360713d3ef851e9f283a4f50347dbad
Andranik <tednaaa@gmail.com>
2024-08-19 23:33:55 +0400

    nvim: disable nvim-highlight-colors virtual symbol rendering

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,8 +8,8 @@ return {
 		"brenoprata10/nvim-highlight-colors",
 		config = function()
 			require("nvim-highlight-colors").setup({
-				virtual_symbol = "●",
-				render = "virtual",
+				-- virtual_symbol = "●",
+				-- render = "virtual",
 			})
 		end,
 	},
