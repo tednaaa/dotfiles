c3860957f8fa328801421f04f7ec5eedfa6043c2
Andranik <tednaaa@gmail.com>
2024-08-22 20:37:35 +0400

    nvim: add gitsigns number highlight

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -22,6 +22,7 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup({
+				numhl = true,
 				signs = {
 					add = { text = "▎" },
 					change = { text = "▎" },
