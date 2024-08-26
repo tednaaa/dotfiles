845d889556c668456ad3fd7240c522d2faa69158
Andranik <tednaaa@gmail.com>
2024-08-26 12:46:07 +0400

    nvim(conform): use custom autocmd instead of eslint_d

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,15 +1,29 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			typescript = { "prettierd", "eslint_d" },
-			typescriptreact = { "prettierd", "eslint_d" },
-			javascript = { "prettierd", "eslint_d" },
-			javascriptreact = { "prettierd", "eslint_d" },
-			vue = { "prettierd", "eslint_d" },
-			json = { "prettierd" },
-		},
-		format_on_save = { timeout_ms = 1000, lsp_format = "fallback" },
-	},
+	lazy = false,
+	config = function()
+		require("conform").setup({
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			notify_no_formatters = true,
+			notify_on_error = true,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				json = { "jsonlint", "prettierd" },
+				dockerfile = { "hadolint" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				svelte = { "prettierd" },
+				vue = { "prettierd" },
+				go = { "golines", "goimports", "golangci", "golangci_lint_ls", "golangci-lint" },
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = vim.api.nvim_create_augroup("EslintFixAll", { clear = true }),
+			pattern = { "*.tsx", "*.ts", "*.jsx", "*.js", "*.vue" },
+			command = "silent! EslintFixAll",
+		})
+	end,
 }
