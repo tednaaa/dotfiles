df9f8e0a18131f79c2a422c0164c80d91414c585
Andranik <tednaaa@gmail.com>
2024-08-26 23:40:18 +0400

    yazi: add config for fish also

--- fish/config.fish
+++ fish/config.fish
@@ -20,3 +20,12 @@ end
 starship init fish | source
 fzf --fish | source
 
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
