ce5abcdbdc1dd6502d6a59bffc187dce01a5f54a
Andranik <tednaaa@gmail.com>
2024-08-26 14:01:46 +0400

    fish: add pnpm config

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,19 @@ if status is-interactive
 end
 set -x XDG_CONFIG_HOME $HOME/.config
 
+source ~/.cargo/env.fish
 source ~/.asdf/asdf.fish
-source "$HOME/.cargo/env.fish"
 source ~/.asdf/plugins/golang/set-env.fish
 
 set -x ASDF_GOLANG_MOD_VERSION_ENABLED true
 
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+set -gx PNPM_HOME "/Users/tednaaa/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
 
 starship init fish | source
 fzf --fish | source
