83949470bee39604b53f16be75b7aba6686f309f
Andranik <tednaaa@gmail.com>
2024-08-27 23:42:09 +0400

    nvim: configure formatter for go

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -10,6 +10,7 @@ return {
 				javascriptreact = { "prettierd", "eslint_d" },
 				vue = { "prettierd", "eslint_d" },
 				json = { "prettierd" },
+				go = { "gofumpt", "golines", "gomodifytags", "gotests" },
 			},
 			format_after_save = { async = true, lsp_format = "fallback" },
 		})
