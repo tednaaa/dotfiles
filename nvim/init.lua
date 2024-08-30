2b14d795011c0bffc632142b0730dd5e540aec47
Andranik <tednaaa@gmail.com>
2024-08-30 15:52:49 +0400

    nvim(lazy): disable some default neovim things

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,8 +20,38 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-	change_detection = {
-		enabled = false,
-		notify = false,
+	change_detection = { enabled = false, notify = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"2html_plugin",
+				"tohtml",
+				"getscript",
+				"getscriptPlugin",
+				"gzip",
+				"logipat",
+				"netrw",
+				"netrwPlugin",
+				"netrwSettings",
+				"netrwFileHandlers",
+				"matchit",
+				"tar",
+				"tarPlugin",
+				"rrhelper",
+				"spellfile_plugin",
+				"vimball",
+				"vimballPlugin",
+				"zip",
+				"zipPlugin",
+				"tutor",
+				"rplugin",
+				"syntax",
+				"synmenu",
+				"optwin",
+				"compiler",
+				"bugreport",
+				"ftplugin",
+			},
+		},
 	},
 })
