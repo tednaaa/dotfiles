30feb58766169570f183f0b93b6f4d962262bc92
Andranik <tednaaa@gmail.com>
2024-09-01 23:48:21 +0400

    nvim(conform): change to format_on_save

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -12,7 +12,7 @@ return {
 				json = { "prettierd" },
 				go = { "gofumpt", "golines" },
 			},
-			format_after_save = { async = true, lsp_format = "fallback" },
+			format_on_save = { lsp_format = "fallback", timeout_ms = 500 },
 		})
 	end,
 }
