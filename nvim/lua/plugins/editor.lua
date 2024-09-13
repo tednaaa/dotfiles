2eebdb0b8ba84c9fa56b4a526133ddb1a58c9dad
Andranik <tednaaa@gmail.com>
2024-09-13 18:39:07 +0400

    nvim(lualine): remove git diff

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -80,7 +80,7 @@ return {
 				},
 				sections = {
 					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_b = { "branch", "diagnostics" },
 					lualine_c = { { "filename", path = 1 } },
 					lualine_x = { "encoding", "fileformat", "filetype" },
 					lualine_y = { "progress" },
