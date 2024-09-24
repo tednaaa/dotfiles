68b5107d68a22c91ac90f867fb485fab34f3ac2b
Andranik <tednaaa@gmail.com>
2024-09-24 20:58:08 +0400

    wezterm: update window_frame font_size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -26,7 +26,7 @@ config.adjust_window_size_when_changing_font_size = false
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.window_frame = {
-	font_size = 10,
+	font_size = 12,
 }
 
 local act = wezterm.action
