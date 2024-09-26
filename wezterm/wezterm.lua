ee8a528eff5c622dbf90a18606f0fe1666b824c5
Andranik <tednaaa@gmail.com>
2024-09-26 22:26:28 +0400

    wezterm: change font size and weight

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,9 +18,9 @@ config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.color_scheme = "GitHub Dark"
 config.colors = { background = "#141414" }
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
-config.font_size = 12
-config.line_height = 1.24
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+config.font_size = 12.3
+config.line_height = 1.2
 
 config.adjust_window_size_when_changing_font_size = false
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
