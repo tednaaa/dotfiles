27a2f53b01fed491b169e8aeff6f2c2e60cfc767
Andranik <tednaaa@gmail.com>
2024-10-01 20:39:21 +0400

    nvim: add mini icons and mock nvim web devicons

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,4 +6,12 @@ return {
 			require("mini.surround").setup()
 		end,
 	},
+	{
+		"echasnovski/mini.icons",
+		version = false,
+		config = function()
+			require("mini.icons").setup()
+			MiniIcons.mock_nvim_web_devicons()
+		end,
+	},
 }
