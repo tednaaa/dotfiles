e974ddb3482755a197048148733e6a0f15098899
Andranik <tednaaa@gmail.com>
2024-10-25 23:49:53 +0400

    fish: remove go installed by asdf

--- fish/config.fish
+++ fish/config.fish
@@ -4,19 +4,7 @@ end
 
 source ~/.cargo/env.fish
 source ~/.asdf/asdf.fish
-source ~/.asdf/plugins/golang/set-env.fish
-
-set -x ASDF_GOLANG_MOD_VERSION_ENABLED true
 
 starship init fish | source
 fzf --fish | source
-
-function yy
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
 
