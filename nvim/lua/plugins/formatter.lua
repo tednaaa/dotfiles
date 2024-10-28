04289430c82fd96c92bf29f12549aa36b759ee7a
Andranik <tednaaa@gmail.com>
2024-10-28 18:34:09 +0400

    nvim(conform): add ability to disable formatting for buffer or globally

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -9,11 +9,19 @@ return {
 				javascript = { "prettierd", "eslint_d" },
 				javascriptreact = { "prettierd", "eslint_d" },
 				vue = { "prettierd", "eslint_d" },
+				astro = { "prettierd", "eslint_d" },
 				json = { "prettierd" },
 				markdown = { "prettierd" },
 				go = { "gofumpt", "golines" },
 			},
-			format_on_save = { lsp_format = "fallback", timeout_ms = 500 },
+			format_on_save = function(bufnr)
+				-- to disable for current buffer
+				-- lua vim.b.disable_autoformat = true
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
 		})
 	end,
 }
