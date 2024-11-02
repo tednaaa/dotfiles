3e026d1699c0347ed1252f2f8242772a0b76f417
Andranik <tednaaa@gmail.com>
2024-11-02 17:04:41 +0400

    fish: add pnpm

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,10 @@ source ~/.asdf/asdf.fish
 
 starship init fish | source
 
+
+# pnpm
+set -gx PNPM_HOME "/home/tedna/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
