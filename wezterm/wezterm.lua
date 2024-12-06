af90ccad54e964fa7d7cccbaf33628ca4a182f45
Andranik <tednaaa@gmail.com>
2024-12-07 00:47:57 +0400

    wezterm: fix opening in wayland

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,6 +11,8 @@ if wezterm.config_builder then
 	config:set_strict_mode(true)
 end
 
+config.enable_wayland = false
+
 -- auto fullscreen not working with this option
 -- config.window_decorations = "RESIZE"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
