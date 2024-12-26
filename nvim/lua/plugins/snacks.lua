87d7d056c807209a1c11db22dae59a7a51cb3322
Andranik <tednaaa@gmail.com>
2024-12-26 13:54:37 +0400

    nvim(snacks): add keymaps for words prev next refs jump

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,8 +7,12 @@ return {
 		bigfile = { enabled = true },
 		input = { enabled = true },
 		quickfile = { enabled = true },
-		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
+	},
+	-- stylua: ignore
+	keys = {
+		{ "<A-p>", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+		{ "<A-n>", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
 	},
 }
