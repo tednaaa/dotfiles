48812ac038c6e642bf39ef12152680343d538253
Andranik <tednaaa@gmail.com>
2025-06-12 15:35:55 +0400

    nvim: update to blink v1

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -5,32 +5,25 @@ return {
 		opts = { library = { { path = "luvit-meta/library", words = { "vim%.uv" } } } },
 	},
 
-	-- {
-	-- 	"supermaven-inc/supermaven-nvim",
-	-- 	config = function()
-	-- 		require("supermaven-nvim").setup({
-	-- 			keymaps = {
-	-- 				accept_suggestion = "<A-a>",
-	-- 				clear_suggestion = "<A-c>",
-	-- 				accept_word = "<A-j>",
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<A-a>",
+					clear_suggestion = "<A-c>",
+					accept_word = "<A-j>",
+				},
+			})
+		end,
+	},
 
 	{
 		"saghen/blink.cmp",
-		lazy = false,
-		dependencies = "rafamadriz/friendly-snippets",
-		version = "v0.*",
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
+		dependencies = { "rafamadriz/friendly-snippets" },
+
+		version = "1.*",
 		opts = {
-			enabled = function()
-				return not vim.tbl_contains({ "DressingInput" }, vim.bo.filetype)
-					and vim.bo.buftype ~= "prompt"
-					and vim.b.completion ~= false
-			end,
 			keymap = {
 				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 				["<C-e>"] = { "hide" },
@@ -45,36 +38,16 @@ return {
 				["<Tab>"] = { "snippet_forward", "fallback" },
 				["<S-Tab>"] = { "snippet_backward", "fallback" },
 			},
+
 			appearance = { nerd_font_variant = "mono" },
-			completion = {
-				trigger = { show_on_trigger_character = false },
-				documentation = { auto_show = true, auto_show_delay_ms = 200 },
-				menu = {
-					max_height = 15,
-					draw = {
-						treesitter = { "lsp" },
-						components = {
-							kind_icon = {
-								ellipsis = false,
-								text = function(ctx)
-									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-									return kind_icon
-								end,
-								highlight = function(ctx)
-									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-									return hl
-								end,
-							},
-						},
-					},
-				},
-			},
+			completion = { documentation = { auto_show = false } },
+
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
-				-- disable cmdline completions
-				cmdline = {},
 			},
 			signature = { enabled = true },
+
+			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		opts_extend = { "sources.default" },
 	},
