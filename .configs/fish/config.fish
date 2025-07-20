32e827e9461e68b21ac1ffb4f9d0e26afb027e8c
Andranik <tednaaa@gmail.com>
2025-07-20 22:35:45 +0400

    remove useless pnpm comments

--- .configs/fish/config.fish
+++ .configs/fish/config.fish
@@ -10,9 +10,7 @@ source ~/.cargo/env.fish
 ~/.local/bin/mise activate fish | source
 starship init fish | source
 
-# pnpm
 set -gx PNPM_HOME "/home/tedna/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
