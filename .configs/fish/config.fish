d789e906673a87d3d3a11f601aa2b037cc0bc9c4
Andranik <tednaaa@gmail.com>
2025-07-27 21:05:37 +0400

    bruh

--- .configs/fish/config.fish
+++ .configs/fish/config.fish
@@ -11,10 +11,12 @@ alias ls="eza --all --icons -l --no-user -o"
 
 set -gx EDITOR "zed --wait"
 
+# pnpm
 set -gx PNPM_HOME "/home/tedna/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+# pnpm end
 
 source ~/.cargo/env.fish
 ~/.local/bin/mise activate fish | source
