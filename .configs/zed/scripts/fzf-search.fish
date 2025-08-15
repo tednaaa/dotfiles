e242f88a4a38b9a43ea787cf7fddf61a5c1a5373
Andranik <tednaaa@gmail.com>
2025-08-15 17:49:21 +0400

    zed: fzf search

---
+++ .configs/zed/scripts/fzf-search.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/fish
+
+set match (rg --no-heading --line-number --color=always "" \
+	| fzf --ansi --delimiter : \
+		--preview 'set -l line (echo {2}); \
+							 set -l start (math "max(1, $line-10)"); \
+							 set -l end (math $line+10); \
+							 bat --color=always --line-range "$start":"$end" --highlight-line "$line" {1}' \
+		--preview-window=down:wrap:70%)
+
+if test -n "$match"
+	set file (echo $match | cut -d: -f1)
+	set line (echo $match | cut -d: -f2)
+	zed "$file":"$line"
+end
