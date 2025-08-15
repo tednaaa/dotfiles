e888fb8e06826b010ff1c48f90607f637fae1e16
Andranik <tednaaa@gmail.com>
2025-08-15 17:57:24 +0400

    fzf search style plain

--- .configs/zed/scripts/fzf-search.fish
+++ .configs/zed/scripts/fzf-search.fish
@@ -5,7 +5,7 @@ set match (rg --no-heading --line-number --color=always "" \
 		--preview 'set -l line (echo {2}); \
 							 set -l start (math "max(1, $line-10)"); \
 							 set -l end (math $line+10); \
-							 bat --color=always --line-range "$start":"$end" --highlight-line "$line" {1}' \
+							 bat --color=always --style=plain --line-range "$start":"$end" --highlight-line "$line" {1}' \
 		--preview-window=down:wrap:70%)
 
 if test -n "$match"
