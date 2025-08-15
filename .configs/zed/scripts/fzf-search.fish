e7d0029879d49166324f751ec5cf184a1f736d52
Andranik <tednaaa@gmail.com>
2025-08-15 18:51:57 +0400

    fuck

--- .configs/zed/scripts/fzf-search.fish
+++ .configs/zed/scripts/fzf-search.fish
@@ -1,23 +1,10 @@
 #!/usr/bin/fish
 
-set -l ignores \
-	'.git/' \
-	'node_modules/' \
-	'pnpm-lock.yaml' \
-	'package-lock.json' \
-	'dist/' \
-	'build/'
-
-set -l ignore_flags
-for p in $ignores
-	set -a ignore_flags --glob "!$p"
-end
-set -l ignore_str (string join ' ' $ignore_flags)
-
 set -l reload_cmd '
 	set -l q {q}
 	if test -n "$q"
-		rg -i --hidden --no-heading --line-number --color=always '"$ignore_str"' "$q"
+		set -l fuzzy_pattern (string replace -a " " ".*" "$q")
+		rg -i --hidden --no-heading --line-number --color=always "$fuzzy_pattern"
 	else
 		printf ""
 	end
@@ -36,15 +23,16 @@ set -l preview_cmd '
 	set -l start (math "max(1, $line-10)")
 	set -l end (math $line+10)
 	if test -n "$q"
+		set -l fuzzy_pattern (string replace -a " " ".*" "$q")
 		bat --color=always --style=plain --line-range "$start":"$end" "$file" \
-		| rg -i --color=always --colors "match:bg:yellow" --colors "match:fg:black" --passthru -- "$q"
+		| rg -i --color=always --colors "match:bg:yellow" --colors "match:fg:black" --passthru -- "$fuzzy_pattern"
 	else
 		bat --color=always --style=plain --line-range "$start":"$end" "$file"
 	end
 '
 
 set -l match (
-	printf "" | fzf --ansi --disabled --prompt 'rg> ' \
+	printf "" | fzf --ansi --disabled --prompt 'fuzzy rg> ' \
 		--delimiter : --nth=3.. \
 		--bind "change:reload:$reload_cmd" \
 		--preview "$preview_cmd" \
@@ -54,5 +42,5 @@ set -l match (
 if test -n "$match"
 	set -l file (echo $match | cut -d: -f1)
 	set -l line (echo $match | cut -d: -f2)
-	zed "$file":"$line"
+	zed "$file:$line"
 end
