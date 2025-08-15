59d7229ef03be05048cd05ff7ab343e0e1c58fca
Andranik <tednaaa@gmail.com>
2025-08-15 18:02:39 +0400

    very good fzf search highlights

--- .configs/zed/scripts/fzf-search.fish
+++ .configs/zed/scripts/fzf-search.fish
@@ -2,10 +2,12 @@
 
 set match (rg --no-heading --line-number --color=always "" \
 	| fzf --ansi --delimiter : \
-		--preview 'set -l line (echo {2}); \
-							 set -l start (math "max(1, $line-10)"); \
-							 set -l end (math $line+10); \
-							 bat --color=always --style=plain --line-range "$start":"$end" --highlight-line "$line" {1}' \
+		--preview 'set -l file {1}; \
+					set -l line {2}; \
+					set -l query {q}; \
+					set -l start (math "max(1, $line-10)"); \
+					set -l end (math $line+10); \
+					bat --color=always --style=plain --line-range "$start":"$end" "$file" | rg --color=always --colors "match:bg:yellow" --colors "match:fg:black" --passthru "$query" || bat --color=always --style=plain --line-range "$start":"$end" "$file"' \
 		--preview-window=down:wrap:70%)
 
 if test -n "$match"
