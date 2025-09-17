ea00263b9496f38a23d712f7d9c07b687b36c640
Andranik <tednaaa@gmail.com>
2025-09-17 22:59:56 +0400

    configure fzf lua

--- .configs/nvim/lua/plugins/fzf.lua
+++ .configs/nvim/lua/plugins/fzf.lua
@@ -11,5 +11,22 @@ return {
         -- horizontal = "right:60%",
       },
     },
+
+    grep = {
+      actions = {
+        ["ctrl-g"] = false,
+        ["alt-f"] = { require("fzf-lua.actions").grep_lgrep },
+      },
+    },
+
+    oldfiles = { cwd_only = true },
+  },
+  keys = {
+    {
+      "<leader>/",
+      function()
+        require("fzf-lua").grep_project()
+      end,
+    },
   },
 }
