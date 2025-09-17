2bb4a4f4e5cf7252b500ea71b1969bc986734213
Andranik <tednaaa@gmail.com>
2025-09-17 19:33:35 +0400

    add lazyvim

--- .configs/nvim/lua/core/user_commands.lua
+++
@@ -1,5 +0,0 @@
-local generate_editorconfig = require("modules.generate-editorconfig")
-
-vim.api.nvim_create_user_command("GenerateEditorconfig", function()
-	generate_editorconfig.generate_editorconfig()
-end, {})
--- .configs/nvim/scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-rm -rf ~/.local/share/nvim && echo -e "${GREEN}~/.local/share/nvim has been cleared successfully.${NC}"
-rm -rf ~/.local/state/nvim && echo -e "${GREEN}~/.local/state/nvim has been cleared successfully.${NC}"
-rm -rf ~/.cache/nvim && echo -e "${GREEN}~/.cache/nvim has been cleared successfully.${NC}"
